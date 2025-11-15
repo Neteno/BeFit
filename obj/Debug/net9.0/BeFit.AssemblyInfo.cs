4b9a28edafb9df76ee2639b922f23c8a7bd043c9
Neteno <szymon.andrzejczak@spoko.pl>
2025-11-15 16:32:25 +0100

    Delete Folder Migrations

--- obj/Debug/net9.0/BeFit.AssemblyInfo.cs
+++ obj/Debug/net9.0/BeFit.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501f8b86ff6a8f675c78e9785c8c09db265a1bb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51c966bb494420ff4dd0c0b03a9e8a2cc1a9135")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
