007f41d1fa34eabfae8453029e01e1242fb89472
Neteno <szymon.andrzejczak@spoko.pl>
2025-11-15 16:38:31 +0100

    Add 3 models

---
+++ Models/ExerciseEntry.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeFit.Models
+{
+    public class ExerciseEntry
+    {
+        public int Id { get; set; }
+
+        // Relacja do TrainingSession
+        [Required]
+        public int TrainingSessionId { get; set; }
+        public TrainingSession? TrainingSession { get; set; }
+
+        // Relacja do ExerciseType
+        [Required]
+        public int ExerciseTypeId { get; set; }
+        public ExerciseType? ExerciseType { get; set; }
+
+        // Obciążenie w kg (może być 0 jeśli ćwiczenie bez obciążenia)
+        [Range(0, 10000)]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Weight { get; set; }
+
+        [Range(1, 100)]
+        public int Sets { get; set; }
+
+        [Range(1, 1000)]
+        public int Reps { get; set; }
+    }
+}
---
+++ Models/TrainingSession.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeFit.Models
+{
+    public class TrainingSession
+    {
+        public int Id { get; set; }
+        [Display(Name = "Data i godzina rozpoczęcia")]
+        [Required]
+        public DateTime Start { get; set; }
+
+        [Display(Name = "Data i godzina zakończenia")]
+        [Required]
+        public DateTime End { get; set; }
+        public List<ExerciseEntry> ExerciseEntries { get; set; } = new();
+    }
+}
