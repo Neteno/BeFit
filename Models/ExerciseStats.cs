8c4d6ebd4e14eb481226f5d753abe7a62c011679
Neteno <szymon.andrzejczak@spoko.pl>
2025-11-17 17:44:35 +0100

    Add ExerciseStats.cs

---
+++ Models/ExerciseStats.cs
@@ -0,0 +1,11 @@
+﻿namespace BeFit.Models
+{
+    public class ExerciseStats
+    {
+        public string ExerciseName { get; set; }
+        public int Count { get; set; }
+        public int TotalReps { get; set; }
+        public decimal AvgWeight { get; set; }
+        public decimal MaxWeight { get; set; }
+    }
+}
