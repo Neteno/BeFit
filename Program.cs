0ad194c6d1af42db37befc678c1d9911a3442e6e
Neteno <szymon.andrzejczak@spoko.pl>
2025-11-19 12:25:13 +0100

    Add new model AppUser,Edit Add AppUser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BeFit.Data;
+using BeFit.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +18,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
