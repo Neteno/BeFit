a96950e2c9b1ba6535919f98fa9fd5d8251fac8d
Neteno <szymon.andrzejczak@spoko.pl>
2025-11-21 15:41:43 +0100

    Fix

--- obj/Debug/net9.0/BeFit.AssemblyInfo.cs
+++ obj/Debug/net9.0/BeFit.AssemblyInfo.cs
@@ -16,7 +16,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc001239b929828cf2a5c2d1285d04844dde6a2")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8bb754a9c0ef0bc49addaff0441b6a05a5aa9b0")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("BeFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
